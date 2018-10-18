ad7e323da37389351ab361299cf72b161533152f
nhatminhp <nmp.tb.212@gmail.com>
2018-10-18 12:10:28 +0200

    Update app.py

--- BENCHOP/app.py
+++ BENCHOP/app.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 def runbenchmark():
   results = runBench.delay()
   resultat = results.get()
+  return resultat
 
 if __name__ == '__main__'
   app.run(host='0.0.0.0', debug=True)
