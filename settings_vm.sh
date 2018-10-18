3e88bbdc09aabb594c749d3086b5515cf5259d1e
nhatminhp <nmp.tb.212@gmail.com>
2018-10-18 11:01:45 +0200

    Update settings_vm.sh

--- settings_vm.sh
+++ settings_vm.sh
@@ -1,15 +1,14 @@
 #Settings for vm
 
 sudo apt-get update
-sudo apg-get upgrade
-sudo apt-get install python-pip
+sudo apt-get install -y python-pip
 sudo pip install -U Celery
 sudo apt install -y python-celery-common
 sudo pip install flask
 sudo apt-get install -y octave
 sudo pip install oct2py
 sudo apt-get install -y python-scipy
-sudo apt install rabbitmq-server
+sudo apt install -y rabbitmq-server
 
 # Username: ACC15
 # Password: pwd
