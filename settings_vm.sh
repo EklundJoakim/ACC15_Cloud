ebcad8706ede482b7a381233322c1d0a4f0bcfd4
nhatminhp <nmp.tb.212@gmail.com>
2018-10-22 13:10:29 +0000

    fix file .sh

--- settings_vm.sh
+++ settings_vm.sh
@@ -1,13 +1,14 @@
-#Settings for vm
-
 sudo apt-get update
-sudo apt-get install -y python-pip
-sudo pip install -U Celery
+sudo apt-get install -y python3-pip
+export LC_ALL=C
+sudo pip3 install -U Celery
 sudo apt install -y python-celery-common
-sudo pip install flask
+sudo pip3 install flask
+sudo pip3 install numpy
 sudo apt-get install -y octave
-sudo pip install oct2py
-sudo apt-get install -y python-scipy
+sudo pip3 install oct2py
+sudo pip3 install scipy
+sudo apt get install -y pyhton3-opestackclient
 sudo apt install -y rabbitmq-server
 
 # Username: ACC15
@@ -18,4 +19,5 @@ sudo apt install -y rabbitmq-server
 sudo rabbitmqctl add_user ACC15 pwd
 sudo rabbitmqctl add_vhost myvhost
 sudo rabbitmqctl set_user_tags ACC15 mytag
-sudo rabbitmqctl set_permissions -p myvhost ACC15 ".*" ".*" ".*"
+sudo rabbitmqctl set_permissions -p myvhost ACC15 .* .* .*
+
