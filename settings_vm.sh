107df81a10316d1720bf8900a9def2b5348fa372
EklundJoakim <joakimeklund93@gmail.com>
2018-10-25 14:38:48 +0200

    Update settings_vm.sh

--- settings_vm.sh
+++ settings_vm.sh
@@ -10,6 +10,7 @@ sudo apt-get install -y octave
 sudo pip3 install oct2py
 sudo pip3 install scipy
 sudo apt-get install -y pyhton3-opestackclient
+sudo apt install -y docker.io
 sudo apt install -y rabbitmq-server
 
 # Username: ACC15
